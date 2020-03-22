94cc9d46f4897d7ad7fbfa1aa57e4bc1505b920b
Kirill <49115928+kirill-efimov-sk@users.noreply.github.com>
2020-03-23 00:00:23 +0300

    init: add code
    createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,8 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+  module.exports = function createDreamTeam(members) {
+    if (!Array.isArray(members)) {return false}
+    else {
+    return members.map(member => {if (typeof member === 'string') {
+      return member.trim().charAt(0).toUpperCase();
+      }
+    }).sort().join('');
+  }};
